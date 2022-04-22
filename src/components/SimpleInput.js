34abbd881c35247572e39fefa86093c9b5fa23f9
hari07 <hariodukkathil@gmail.com>
2022-04-22 13:22:15 +0530

    validation v1 ;

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,16 @@
-import { useState, useRef } from "react";
+import { useState, useRef ,useEffect} from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-
+  const [isValid,setIsValid] = useState(false)
+  const [isTouched,setIsTouched] = useState(false)
   const nameRef = useRef();
+
+  useEffect(()=>{
+    if(isValid){
+      console.log('A name has been passed');
+    }
+  },[isValid])
 
   const onNameChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -11,6 +18,12 @@ const SimpleInput = (props) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    setIsTouched(true)
+    if(enteredName.trim() === ''){
+      setIsValid(false)
+      return;
+    }
+    setIsValid(true)
     console.log(enteredName);
 
     const value = nameRef.current.value
@@ -18,9 +31,15 @@ const SimpleInput = (props) => {
 
     setEnteredName(' ')
   };
+
+  const isEnteredInvalid = isTouched && !isValid
+
+  const formClass = isEnteredInvalid ? 'form-control invalid' : 'form-control '
+
+
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <div className={formClass}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameRef}
@@ -30,6 +49,7 @@ const SimpleInput = (props) => {
           value={enteredName}
         />
       </div>
+      {isEnteredInvalid && <p className="error-text">A name is required</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
